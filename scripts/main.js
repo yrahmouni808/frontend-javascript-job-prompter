4def518a30f4cddfca0aa7ef0f018f0c6567061a
yrahmouni808 <neonidas2004@gmail.com>
2025-04-02 13:13:13 +0200

    Opdracht 1 afgemaakt.

--- scripts/main.js
+++ scripts/main.js
@@ -71,4 +71,8 @@ const departments = {
     }
 }
 
-console.log(departments);
+//console.log(departments);
+console.log(`De afdeling Sales heeft ${departments.sales.numberOfEmployees} medewerkers.`);
+console.log(`Marketing is een leuke afdeling om te werken. ${departments.marketing.description}.`);
+console.log(`De afdeling Customer Service heeft ${departments["customer-service"].numberOfEmployees} medewerkers.`);
+console.log(`Sales is een uitdagende afdeling om te werken als Verkoopmanager. ${departments.sales.jobs[1].description}`);
